d0ef49f7d752e3e9c2d498afe86470b9ce6cb7a1
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-13 22:30:51 +0530

    remove boiler-plate files; add hitman component

---
+++ myapp/src/Hitman.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Hitman extends Component {
+  render() {
+    return (
+      <div className="ninja">
+        <div>Name: Agent 47</div>
+        <div>Age: 35</div>
+        <div>Rank: Codename 47</div>
+      </div>
+    );
+  }
+}
+
+export default Hitman;
