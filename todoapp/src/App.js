66cf3ea43944be2b434041f418a798690645b522
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-14 21:52:54 +0530

    Add UI and ability to delete todo

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './Todos';
 
-
-function App() {
+class App extends Component {
   state = {
     todos: [
       { id: 1, content: 'buy some chocolate' },
       { id: 2, content: 'play mario kart' }
     ]
   }
-  return (
-    <div className="App">
 
-    </div>
-  );
+  deleteTodo = (id) => {
+    // console.log(id);
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+
+    this.setState({ todos });
+
+
+  }
+
+  render() {
+    return (
+      <div className="todo-app container" >
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
