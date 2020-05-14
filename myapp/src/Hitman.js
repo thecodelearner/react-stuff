8c555e1b96e7febc1dc262ead7068a487f6eb052
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-14 13:03:20 +0530

    changed functional class to class ui component

--- myapp/src/Hitman.js
+++ myapp/src/Hitman.js
@@ -1,23 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Hitman extends Component {
-  render() {
-    // console.log(this.props);
-    const { hitmans } = this.props;
+const Hitman = ({ hitmans }) => {
+  const hitmanList = hitmans.map((hitman) => {
+    return (
+      <div className="hitman" key={hitman.id}>
+        <div>Name: {hitman.name}</div>
+        <div>Age: {hitman.age}</div>
+        <div>Alias: {hitman.alias}</div>
+        <br />
+      </div>
+    );
+  });
 
-    const hitmanList = hitmans.map((hitman) => {
-      return (
-        <div className="hitman" key={hitman.id}>
-          <div>Name: {hitman.name}</div>
-          <div>Age: {hitman.age}</div>
-          <div>Alias: {hitman.alias}</div>
-          <br />
-        </div>
-      );
-    });
-
-    return <div className="hitman-list">{hitmanList}</div>;
-  }
-}
+  return <div className="hitman-list">{hitmanList}</div>;
+};
 
 export default Hitman;
