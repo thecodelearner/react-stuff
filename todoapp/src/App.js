a38d32a75f915c82cf1dd5bbff05c749271dc1d8
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-14 21:24:51 +0530

    create and init new todo app.

---
+++ todoapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function App() {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some chocolate' },
+      { id: 2, content: 'play mario kart' }
+    ]
+  }
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
